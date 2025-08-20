cd4621db1f8591f46342a92eb1accc29794facc9
Dmytro Furtune <d.furtune18@gmail.com>
2025-08-20 20:04:49 +0300

    add to CI lint and format

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,18 +1,6 @@
-import { useEffect } from 'react';
 import Button from '../../components/Button/Button';
 
 const HomePage = () => {
-  useEffect(() => {
-    if (import.meta.env.DEV) {
-      /* eslint-disable no-console */
-      console.debug('[HomePage] component mounted');
-      console.debug(
-        '[HomePage] This is a simple log message for debugging purposes.'
-      );
-      /* eslint-enable no-console */
-    }
-  }, []);
-
   return (
     <div>
       <h1>Домашня сторінка</h1>
