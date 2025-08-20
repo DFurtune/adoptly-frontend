461485bbdff6d25648d24fe45b24d1605321aee6
Dmytro Furtune <d.furtune18@gmail.com>
2025-08-20 19:39:57 +0300

    add logs dor coderabbit

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,12 @@
 import Button from '../../components/Button/Button';
 
 const HomePage = () => {
+
+  console.log('HomePage component rendered');
+
+  console.log('This is a simple log message for debugging purposes.');
+  
+  
   return (
     <div>
       <h1>Домашня сторінка</h1>
