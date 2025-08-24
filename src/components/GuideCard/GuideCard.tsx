0eeac940fb8b2e069029391fabbd5e5a06baae59
Dmytro Furtune <d.furtune18@gmail.com>
2025-08-24 15:23:12 +0300

    Refactor GuideCard and HowItWorks components for improved readability and consistency; update CSS variable naming conventions

--- src/components/GuideCard/GuideCard.tsx
+++ src/components/GuideCard/GuideCard.tsx
@@ -1,45 +1,38 @@
-import React from "react";
+import React from 'react';
 
-import "./GuideCard.css";
+import './GuideCard.css';
 
 interface GuideCardProps {
-    cardImgSrc: string;
-    cardImgAlt: string;
-    cardNumber: string;
-    cardStep: string;
-    cardDescription: { text: string; bold: boolean }[];
+  cardImgSrc: string;
+  cardImgAlt: string;
+  cardNumber: string;
+  cardStep: string;
+  cardDescription: { text: string; bold: boolean }[];
 }
 
 const GuideCard: React.FC<GuideCardProps> = React.memo(
-    ({ cardImgSrc, cardImgAlt, cardNumber, cardStep, cardDescription }) => {
-        if (!cardDescription.length) {
-            return null;
-        }
-
-        return (
-            <article className="guide-card">
-                <div>
-                    <img
-                        src={cardImgSrc}
-                        alt={cardImgAlt || "Guide card image"}
-                    />
-                </div>
-                <div className="guide-card-text">
-                    <h3>{cardNumber}</h3>
-                    <h4>{cardStep}</h4>
-                    <p>
-                        {cardDescription.map((part, index: number) =>
-                            part.bold ? (
-                                <strong key={index}>{part.text}</strong>
-                            ) : (
-                                part.text
-                            )
-                        )}
-                    </p>
-                </div>
-            </article>
-        );
+  ({ cardImgSrc, cardImgAlt, cardNumber, cardStep, cardDescription }) => {
+    if (!cardDescription.length) {
+      return null;
     }
+
+    return (
+      <article className="guide-card">
+        <div>
+          <img src={cardImgSrc} alt={cardImgAlt || 'Guide card image'} />
+        </div>
+        <div className="guide-card-text">
+          <h3>{cardNumber}</h3>
+          <h4>{cardStep}</h4>
+          <p>
+            {cardDescription.map((part, index: number) =>
+              part.bold ? <strong key={index}>{part.text}</strong> : part.text
+            )}
+          </p>
+        </div>
+      </article>
+    );
+  }
 );
 
 export default GuideCard;
