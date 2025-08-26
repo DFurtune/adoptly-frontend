c2ebd7b3cb60bf1198e85d90a7d2d5f84a2acb2f
Svitlana-G <svitlana.golubieva@gmail.com>
2025-08-26 16:33:16 +0200

    Add section our skills in numbers

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,6 +2,7 @@ import './HomePage.css';
 import Header from '../../components/Header/Header';
 import HowItWorks from '../../components/HowItWorks/HowItWorks';
 import HeroSection from '../../components/HeroSection/HeroSection';
+import OurSkillsInNumbers from '../../components/OurSkillsInNumbers/OurSkillsInNumbers';
 
 const HomePage = () => {
   return (
@@ -10,6 +11,7 @@ const HomePage = () => {
       <div className="container">
         <HeroSection />
         <HowItWorks />
+        <OurSkillsInNumbers />
       </div>
     </main>
   );
