3bb7f1a07f91e03f7a58ab4fa0599fc7d59d4e5b
Svitlana-G <svitlana.golubieva@gmail.com>
2026-05-25 19:42:07 +0200

    Localize shelter sidebar menu

--- src/components/ShelterSidebar/ShelterSidebar.tsx
+++ src/components/ShelterSidebar/ShelterSidebar.tsx
@@ -1,22 +1,25 @@
 import { NavLink } from 'react-router-dom';
 import './ShelterSidebar.css';
+import { useTranslation } from 'react-i18next';
 
 const ShelterSidebar = () => {
+  const { t } = useTranslation();
   return (
-    <nav className="shelter-sidebar">
-      <h2>Shelter Name</h2>
+    <nav className="shelter-sidebar" aria-label={t('shelter.menu.ariaLabel')}>
+      {/* TODO: replace with real shelter name from backend */}
+      <h2>Назва притулку</h2>
       <ul>
         <li>
-          <NavLink to="profile">Профіль притулку</NavLink>
+          <NavLink to="profile">{t('shelter.menu.profile')}</NavLink>
         </li>
         <li>
-          <NavLink to="pets">Картки тварин</NavLink>
+          <NavLink to="pets">{t('shelter.menu.pets')}</NavLink>
         </li>
         <li>
-          <NavLink to="applications">Заявки на адопцію</NavLink>
+          <NavLink to="applications">{t('shelter.menu.applications')}</NavLink>
         </li>
         <li>
-          <NavLink to="analytics">Аналітика</NavLink>
+          <NavLink to="analytics">{t('shelter.menu.analytics')}</NavLink>
         </li>
       </ul>
     </nav>
