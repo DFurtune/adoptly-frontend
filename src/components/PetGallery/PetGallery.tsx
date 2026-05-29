75225fdc826840c14a95097f8917129e3799a844
Svitlana-G <svitlana.golubieva@gmail.com>
2026-05-29 15:28:45 +0200

    Add pet detail page with gallery

--- src/components/PetGallery/PetGallery.tsx
+++ src/components/PetGallery/PetGallery.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Swiper as SwiperClass } from 'swiper/types';
-import { Navigation, Thumbs, FreeMode } from 'swiper/modules';
+import { Thumbs, FreeMode } from 'swiper/modules';
 import 'swiper/css/bundle';
 import './PetGallery.css';
+import { useMediaQuery } from 'react-responsive';
+import { Icon } from '../Icon/Icon';
 
 type PetGalleryProps = {
   images: string[];
@@ -12,13 +14,34 @@ type PetGalleryProps = {
 
 const PetGallery: React.FC<PetGalleryProps> = ({ images, altText }) => {
   const [thumbsSwiper, setThumbsSwiper] = useState<SwiperClass | null>(null);
+  const [isBeginning, setIsBeginning] = useState(true);
+  const [isEnd, setIsEnd] = useState(false);
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const mainSwiper = React.useRef<SwiperClass | null>(null);
+
+  const handlePrev = () => {
+    mainSwiper.current?.slidePrev();
+  };
+  const handleNext = () => {
+    mainSwiper.current?.slideNext();
+  };
+  const handleSlideChange = (swiper: SwiperClass) => {
+    setIsBeginning(swiper.isBeginning);
+    setIsEnd(swiper.isEnd);
+  };
+
   return (
     <div className="pet-gallery">
       <Swiper
-        modules={[Navigation, Thumbs]}
-        navigation
+        modules={[Thumbs]}
         thumbs={{ swiper: thumbsSwiper }}
-        spaceBetween={10}
+        spaceBetween={4}
+        onSwiper={swiper => {
+          mainSwiper.current = swiper;
+          setIsBeginning(swiper.isBeginning);
+          setIsEnd(swiper.isEnd);
+        }}
+        onSlideChange={handleSlideChange}
         className="pet-gallery-main"
       >
         {images.map((img, index) => (
@@ -27,21 +50,49 @@ const PetGallery: React.FC<PetGalleryProps> = ({ images, altText }) => {
           </SwiperSlide>
         ))}
       </Swiper>
-      <Swiper
-        modules={[Thumbs, FreeMode]}
-        onSwiper={setThumbsSwiper}
-        spaceBetween={10}
-        slidesPerView={3}
-        freeMode
-        watchSlidesProgress
-        className="pet-gallery-thumbs"
-      >
-        {images.map((img, index) => (
-          <SwiperSlide key={img}>
-            <img src={img} alt={`${altText} thumbnail ${index + 1}`} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      {images.length > 1 && (
+        <div className="pet-gallery-thumbs-wrapper">
+          {!isMobile && (
+            <button
+              type="button"
+              onClick={handlePrev}
+              className={isBeginning ? 'pet-gallery-nav-btn--disabled' : ''}
+              aria-label="Попереднє фото"
+            >
+              <Icon id="icon-left" size={42} height={42} />{' '}
+            </button>
+          )}
+          <Swiper
+            modules={[Thumbs, FreeMode]}
+            onSwiper={setThumbsSwiper}
+            spaceBetween={16}
+            slidesPerView="auto"
+            freeMode
+            watchSlidesProgress
+            className="pet-gallery-thumbs"
+          >
+            {images.map((img, index) => (
+              <SwiperSlide key={img}>
+                <img src={img} alt={`${altText} thumbnail ${index + 1}`} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          {!isMobile && (
+            <button
+              type="button"
+              onClick={handleNext}
+              aria-label="Наступне фото"
+            >
+              <Icon
+                id="icon-right"
+                size={42}
+                height={42}
+                className={isEnd ? 'pet-gallery-nav-btn--disabled' : ''}
+              />
+            </button>
+          )}
+        </div>
+      )}
     </div>
   );
 };
