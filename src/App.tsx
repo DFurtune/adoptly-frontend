c9645b9563ae564795189888c2f9d9154fe83659
Svitlana-G <svitlana.golubieva@gmail.com>
2026-08-27 15:41:48 +0200

    Extract routes to AppRoutes component

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,14 @@
-import { lazy } from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage';
-import AboutPage from './pages/AboutPage/AboutPage';
-import HowToHelpPage from './pages/HowToHelpPage/HowToHelpPage';
-import SheltersPage from './pages/SheltersPage/SheltersPage';
-import ContactPage from './pages/ContactPage/ContactPage';
-import NotFound from './pages/NotFound/NotFound';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import PetsPage from './pages/PetsPage/PetsPage';
-import PolicyPage from './pages/PolicyPage/PolicyPage';
-import LanguageLayout from './components/LanguageLayout/LanguageLayout';
-import ShelterLayout from './components/ShelterLayout/ShelterLayout';
-import PetDetailPage from './pages/PetDetailPage/PetDetailPage';
-import ForgotPasswordForm from './components/ForgotPasswordForm/ForgotPasswordForm';
-
-const ShelterProfilePage = lazy(
-  () => import('./pages/shelter/ShelterProfilePage/ShelterProfilePage')
-);
-const ShelterPetsPage = lazy(
-  () => import('./pages/shelter/ShelterPetsPage/ShelterPetsPage')
-);
-const ShelterApplicationsPage = lazy(
-  () =>
-    import('./pages/shelter/ShelterApplicationsPage/ShelterApplicationsPage')
-);
-const ShelterAnalyticsPage = lazy(
-  () => import('./pages/shelter/ShelterAnalyticsPage/ShelterAnalyticsPage')
-);
+import AppRoutes from './routes/AppRoutes';
 
 function App() {
   return (
     <Router basename="/adoptly-frontend">
       <Header />
       <main>
-        <Routes>
-          <Route path="/" element={<Navigate to="/uk/" replace />} />
-          <Route path="/:lng" element={<LanguageLayout />}>
-            <Route index element={<HomePage />} />
-            <Route path="about" element={<AboutPage />} />
-            <Route path="how-to-help" element={<HowToHelpPage />} />
-            <Route path="shelters" element={<SheltersPage />} />
-            <Route path="contacts" element={<ContactPage />} />
-            <Route path="pets" element={<PetsPage />} />
-            <Route path="privacy-policy" element={<PolicyPage />} />
-            <Route path="pets/:id" element={<PetDetailPage />} />
-            <Route path="forgot-password" element={<ForgotPasswordForm />} />
-
-            <Route path="shelter" element={<ShelterLayout />}>
-              <Route index element={<Navigate to="profile" replace />} />
-              <Route path="profile" element={<ShelterProfilePage />} />
-              <Route path="pets" element={<ShelterPetsPage />} />
-              <Route
-                path="applications"
-                element={<ShelterApplicationsPage />}
-              />
-              <Route path="analytics" element={<ShelterAnalyticsPage />} />
-            </Route>
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
+        <AppRoutes />
       </main>
       <Footer />
     </Router>
